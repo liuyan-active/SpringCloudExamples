67bce040d9f7022cbb91844ce8cc187966b240f8
闫鹏磊 <75997533@qq.com>
2017-11-23 10:04:29 +0800

    SpringCloud 服务的注册与发现（Eureka）

--- spring-cloud-eureka-client/src/main/java/io/ymq/example/eureka/client/EurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/io/ymq/example/eureka/client/EurekaClientApplication.java
@@ -1,11 +1,9 @@
 package io.ymq.example.eureka.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,13 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
-	}
-
     @RequestMapping("/")
     public String home() {
         return "Hello world";
     }
 
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
 }
