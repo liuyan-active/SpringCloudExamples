f35a175ef340690331619e377a531802956797e3
admin <admin@souyunku.com>
2017-12-10 22:39:37 +0800

    Spring Cloud（六）服务网关 zuul 快速入门

--- spring-cloud-eureka-provider-2/src/test/java/io/ymq/example/eureka/provider/SpringCloudEurekaProviderApplicationTests.java
+++ spring-cloud-eureka-provider-2/src/test/java/io/ymq/example/eureka/provider/SpringCloudEurekaProviderApplicationTests.java
@@ -1,4 +1,4 @@
-package io.ymq.example.eureka;
+package io.ymq.example.eureka.provider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class SpringCloudEurekaProviderApplicationTests {
 
 	@Test
 	public void contextLoads() {
