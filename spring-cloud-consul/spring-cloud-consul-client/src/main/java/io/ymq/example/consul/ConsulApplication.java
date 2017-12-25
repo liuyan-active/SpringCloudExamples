d3e171ecb334f47230364af8b157993ca3a40417
闫鹏磊 <75997533@qq.com>
2017-12-25 23:38:54 +0800

    Spring Cloud（二）Consul 服务治理实现

--- spring-cloud-consul/spring-cloud-consul-client/src/main/java/io/ymq/example/consul/ConsulApplication.java
+++ spring-cloud-consul/spring-cloud-consul-client/src/main/java/io/ymq/example/consul/ConsulApplication.java
@@ -2,7 +2,6 @@ package io.ymq.example.consul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
